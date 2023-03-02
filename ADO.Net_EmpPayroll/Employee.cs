d5b963cc556ad9a902cec0feebc8c32217c993bb
shrutichitriv <shrutichitriv199620@gmail.com>
2023-03-02 17:37:32 +0530

    [Shruti] Perform Employee payroll system ADO.Net program

---
+++ ADO.Net_EmpPayroll/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.Net_EmpPayroll
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long Salary { get; set; }
+        public string MobileNo { get; set; }
+
+    }
+}
---
+++ ADO.Net_EmpPayroll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ADO.Net_EmpPayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO.net Employee payroll system");
+            //Read Operation
+
+            //CrudOperations crud = new CrudOperations();
+            //crud.ReadEmployeeData();
+
+        }
+    }
+}
+
